abc71d4296680b56ebeb274694f560779d030f9e
Collin Stasiak <collinstasiak@gmail.com>
2024-03-02 18:10:45 -0500

    controller works w/ db

--- controllers/ticket-controller.js
+++ controllers/ticket-controller.js
@@ -1,12 +1,20 @@
 const ticketController = {};
-const {Ticket} = require('../database.js');
+const { Ticket } = require('../database.js');
 
-ticketController.createTicket = (req, res) => {
-  Ticket.create(req.body).then(result => res.json(result));
+ticketController.createTicket = async (req, res) => {
+  let response = await Ticket.create(req.body);
+  res.json(response);
 }
 
 ticketController.getAll = async (req, res) => {
-  return await Ticket.findAll();
+  let tickets = await Ticket.findAll();
+  console.log(tickets);
+  res.json(tickets);
+}
+
+ticketController.getOne = async (req, res) => {
+  let id = req.params.id;
+  Ticket.findByPK(id)
 }
 
 module.exports = ticketController;
--- routes/tickets.js
+++ routes/tickets.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const Ticket = require('../models/Ticket');
 const ticketController = require('../controllers/ticket-controller');
 
-// Define routes for ticket management
-
 router.route('/')
-  .post(ticketController.createTicket)
-  .get(ticketController.getAll);
+  .post(async (req, res) => {
+    try {
+      await ticketController.createTicket(req, res);
+    } catch (error) {
+      console.error('Error creating ticket:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  })
+  .get(async (req, res) => {
+    try {
+      await ticketController.getAll(req, res);
+    } catch (error) {
+      console.error('Error fetching tickets:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
 
 module.exports = router;
+
